10a081d3c93463e748f2c5933dd285a8e0528f05
Dhairya Gaur <65876126+Dhairya511@users.noreply.github.com>
2022-05-23 15:36:59 +0530

    Add files via upload

---
+++ Patterns/SWASTIK 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i <= n) {
+
+        int j = 1;
+        while(j <= n) {
+
+        if(i == n/2+1 || j == n/2+1 || (i == 1 && j < n/2+1) || (j == n && i < n/2+1) || (j == 1 && i > n/2+1) || (i == n && j > n/2+1)) {
+            cout << "*" << "  ";
+            }
+            else {
+                cout << "   ";
+            }
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
